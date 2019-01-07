045efbacce0627761ebdd4bdcdd8faab84113a0b
Jacob Pfeifer <jakepf00@gmail.com>
2019-01-07 15:31:44 +0000

    Cleaned up TimerFragment code

--- app/src/main/java/com/jakepf00/cubetimer/fragment/TimerFragment.java
+++ app/src/main/java/com/jakepf00/cubetimer/fragment/TimerFragment.java
@@ -22,13 +22,7 @@ import com.jakepf00.cubetimer.Solve;
 import java.util.ArrayList;
 
 public class TimerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private OnFragmentInteractionListener mListener;
 
     Clock timer = new Clock();
@@ -40,22 +34,12 @@ public class TimerFragment extends Fragment {
     public TimerFragment() {
 
     }
-    // TODO: Rename and change types and number of parameters
-    public static TimerFragment newInstance(String param1, String param2) {
-        TimerFragment fragment = new TimerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static TimerFragment newInstance() {
+        return new TimerFragment();
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -101,20 +85,13 @@ public class TimerFragment extends Fragment {
         listView = getView().findViewById(R.id.session_stats_list);
         listView.setAdapter(arrayAdapter);
     }
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
     @Override
@@ -123,7 +100,6 @@ public class TimerFragment extends Fragment {
         mListener = null;
     }
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
